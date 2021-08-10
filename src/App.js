888ea57a5de96636f2b22dc04d35663ae73d2ab9
Title_of_the_Page <titleofthepage@gmail.com>
2021-08-10 13:36:45 -0400

    Add dark and light mode

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Content } from './components/Content';
+import { Toggle } from './components/Toggle';
+import { useDarkMode } from './styles/useDarkMode';
+import { GlobalStyles, lightTheme, darkTheme } from './styles/globalStyles';
+import styled, { ThemeProvider } from 'styled-components';
+
+const Container = styled.div`
+  max-width: 50%;
+  margin: 8rem auto 0;
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [theme, toggleTheme] = useDarkMode();
+    const themeMode = theme === 'light' ? lightTheme : darkTheme;
+
+    return (
+        <ThemeProvider theme={themeMode}>
+            <Container>
+                <GlobalStyles />
+                <Toggle theme={theme} toggleTheme={toggleTheme} />
+                <Content />
+            </Container>
+        </ThemeProvider>
+    );
 }
 
 export default App;
